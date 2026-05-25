e4c7f382ed37ba92822d50f3407d141f71a30d5f
Emmanuel Oluwatosin <110412246+Tosinuel@users.noreply.github.com>
2026-05-25 16:47:01 +0100

    Refactor dealer and postreview URL patterns

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
@@ -29,6 +28,8 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),  
-        path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',
+         TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>',
+         TemplateView.as_view(template_name="index.html")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
