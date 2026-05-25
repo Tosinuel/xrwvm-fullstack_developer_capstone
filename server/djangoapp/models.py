38366f85265c1fe38d3b5dbcf9830767240935e7
Emmanuel Oluwatosin <110412246+Tosinuel@users.noreply.github.com>
2026-05-25 16:30:47 +0100

    Refactor CarModel and CarMake models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
 
 class CarMake(models.Model):
     """Model representing a car manufacturer/brand"""
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name
 
 
 class CarModel(models.Model):
     """Model representing a specific car model"""
-    # Types of cars
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
@@ -23,8 +21,8 @@ class CarModel(models.Model):
         ('COUPE', 'Coupe'),
         ('CONVERTIBLE', 'Convertible'),
     ]
-    
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(
@@ -34,7 +32,7 @@ class CarModel(models.Model):
             MinValueValidator(2000)
         ]
     )
-    dealer_id = models.IntegerField(null=True, blank=True)  # Reference to dealer in Cloudant
-    
+    dealer_id = models.IntegerField(null=True, blank=True)
+
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.year})"
